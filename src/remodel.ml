8452eb1a3d886c317074413107872c43d84eb2bb
Ted Smith <tedks@riseup.net>
2013-12-08 18:04:12 -0500

    parallel builds

--- src/remodel.ml
+++ src/remodel.ml
@@ -1,7 +1,7 @@
 module L = List
 module H = Hashtbl
 module P = Printf
-module SM = Map.Make(String)
+module IM = Map.Make(struct type t = int let compare = compare end)
 
 module D = Remodel_depgraph
 module C = Remodel_cache
@@ -31,7 +31,7 @@ let remodel_exec_command c =
   | _ -> Failure
 
 let remodel_build dg = 
-  let commands = D.foldi_preorder 
+  let commands = D.foldi_postorder
     (fun acc l n -> match n with 
     | D.Node (_, sc, _) -> begin 
       match sc with None -> acc | Some c -> (l, c)::acc
@@ -39,22 +39,35 @@ let remodel_build dg =
     | _ -> acc) 
     [] dg 
   in 
-  (* build up command lists *)
-  L.iter (fun (l,c) -> match remodel_exec_command c with
-  | Success -> ()
-  | Failure -> failwith (P.sprintf "Error running command %s" c)) 
-    commands
-
-let _ =
-  let prog = Remodel_parse.prog Remodel_lex.file (Lexing.from_channel stdin) in
-  C.init ();
-  let dg = D.make_graph prog in
-  print_endline "Initial graph:";
-  print_endline @@ D.string_of_depgraph dg;
-  let dg' = remodel_filter dg in
-  print_endline "Filtered: "; print_endline @@ D.string_of_depgraph dg';
-  flush_all ();
-  remodel_build dg';
-  remodel_cache dg'
-
+  let cmdmap = 
+    L.fold_left (fun cmap (l, c) ->
+      let level = max_int - l in 
+      let upd = if IM.mem level cmap 
+	then c::IM.find level cmap else [c] in
+      IM.add level upd cmap) IM.empty commands in 
+  IM.iter 
+    (fun l cl -> 
+      let check_build res acc = 
+	match acc with Failure -> acc | Success -> res in 
+      let result = 			(* parallelism goes here *)
+	Par.parmapfold ~ncores:16 remodel_exec_command (Par.L cl)
+	  check_build Success check_build in 
+      match result with 
+      | Success -> ()
+      | Failure -> failwith 
+	(P.sprintf "Couldn't execute commands [%s] on level %d!" 
+	   (String.concat "; " cl) l)
+    ) cmdmap
   
+  let _ =
+    let prog = Remodel_parse.prog Remodel_lex.file (Lexing.from_channel stdin) in
+    C.init ();
+    let dg = D.make_graph prog in
+    print_endline "Initial graph:";
+    print_endline @@ D.string_of_depgraph dg;
+    let dg' = remodel_filter dg in
+    print_endline "Filtered: "; print_endline @@ D.string_of_depgraph dg';
+    flush_all ();
+    remodel_build dg';
+    remodel_cache dg'
+      
